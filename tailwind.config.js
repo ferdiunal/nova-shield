37e44f5b7c335ce5d09d017936634ab18aa8bf97
Ferdi ÜNAL <ferdiunal@outlook.com>
2024-09-16 22:00:37 +0300

    chore: Update NovaShield CSS and Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-
 import { generateTailwindColors } from '../../vendor/laravel/nova/generators'
-// import NovaConfig from '../../vendor/laravel/nova/tailwind.config'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./resources/**/*.{js,vue}'],
-  darkMode: ['class', '[class*="dark"]'],
+  darkMode: ['class', '[class="dark"]'],
   prefix: 'ns-',
   corePlugins: {
     preflight: false,
